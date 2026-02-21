5712f1fb955d4e6e914ba4dd43d734827833cbfb
Marvel123g <marvellousolorunwa23@gmail.com>
2026-02-21 21:05:10 +0100

    Created the dashboard

---
+++ frontend/src/components/Assessment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+
+export default function Assessment() {
+  return (
+    <div className="container">
+      <Sidebar />
+      <div className="assessment_page">
+        hello im assessment page
+      </div>
+    </div>
+  );
+}
